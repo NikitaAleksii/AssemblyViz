266a6e2b47a1e5d38430a5108f8e6c050569beb7
rovotta <rovotta@davidson.edu>
2026-02-22 11:20:45 -0500

    init file for hymn

---
+++ backend/hymn/__init__.py
@@ -0,0 +1,4 @@
+'''__init__.py is what makes Python treat a folder as a package, meaning you can import from it.
+Without it, Python won't recognize the hymn folder as a module.
+It can be completely empty and still do its job. Later, you can optionally use it to expose a cleaner API, like:
+'''
